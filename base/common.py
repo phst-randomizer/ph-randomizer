a85e1e3395b32b92f85b8072cc467fe87167d906
Mike <mikebuntu68@gmail.com>
2022-01-12 03:07:06 -0500

    Replace python build scripts with `Makefile` targets

--- base/common.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# overlays to be modified. The build script uses this to determine what overlays
-# to decompress/compress when building the base rom patch.
-overlays_to_modify = ("0000", "0022", "0031", "0037")
-
-ARMIPS_EXECUTABLE_PATH = os.environ["ARMIPS_EXECUTABLE_PATH"]
-BLZ_EXECUTABLE_PATH = os.environ["BLZ_EXECUTABLE_PATH"]
-NDSTOOL_EXECUTABLE_PATH = os.environ["NDSTOOL_EXECUTABLE_PATH"]
