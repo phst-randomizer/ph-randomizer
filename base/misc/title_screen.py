996bf773111751067f37b92df84fa9fecdfac4cc
Mike <mikebuntu68@gmail.com>
2022-02-04 13:56:38 -0500

    Support custom palettes

--- base/misc/title_screen.py
+++ base/misc/title_screen.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from PIL import Image, ImageDraw
 import click
 from ndspy import graphics2D, lz10, narc, rom
@@ -10,7 +12,7 @@ class AutoList(list):
         list.__setitem__(self, index, value)
 
 
-def extract_image(image: bytes, palette: list) -> Image:
+def extract_image(image: bytes, palette: list[tuple[int, int, int, int]]) -> Image:
     img = Image.new("RGB", (256, 128), (0, 0, 0))
     for y in range(128):
         for x in range(256):
@@ -19,13 +21,19 @@ def extract_image(image: bytes, palette: list) -> Image:
     return img
 
 
-def insert_image(img: Image, palette: list):
+def insert_image(img: Image) -> tuple[bytes, bytes]:
     image = AutoList()
+    palette: list[tuple[int, int, int]] = []
     for y in range(128):
         for x in range(256):
             px = img.getpixel((x, y))
-            image[y * 256 + x] = palette.index(px)
-    return bytes(image)
+            if px in palette:
+                image[y * 256 + x] = palette.index(px)
+            else:
+                palette.append(px)
+                image[y * 256 + x] = len(palette) - 1
+    assert len(palette) <= 256, "Error: image contains more than 256 colors"
+    return bytes(image), graphics2D.savePalette([(r, g, b, 0) for (r, g, b) in palette])
 
 
 @click.command()
@@ -49,7 +57,6 @@ def insert_image(img: Image, palette: list):
 def title_screen(insert: bool, input: str, output: str, image: str, version_string: str):
     nds_rom = rom.NintendoDSRom.fromFile(input)
     narc_file = narc.NARC(lz10.decompress(nds_rom.getFileByName("English/Menu/Tex2D/title.bin")))
-    colors = graphics2D.loadPalette(narc_file.getFileByName("title.ntfp"))
 
     if insert:
         img = Image.open(image).convert("RGB")
@@ -73,20 +80,16 @@ def title_screen(insert: bool, input: str, output: str, image: str, version_stri
                 (x, y), version_string, fill=(214, 41, 41), stroke_width=4, stroke_fill=(0, 0, 0)
             )
 
-        new_image = insert_image(
-            img,
-            [
-                (r, g, b)
-                for (r, g, b, _) in graphics2D.loadPalette(narc_file.getFileByName("title.ntfp"))
-            ],
-        )
+        new_image, new_palette = insert_image(img)
         narc_file.setFileByName("title.ntft", new_image)
+        narc_file.setFileByName("title.ntfp", new_palette)
         nds_rom.setFileByName("English/Menu/Tex2D/title.bin", lz10.compress(narc_file.save()))
         nds_rom.saveToFile(output)
 
     else:
         image_file: bytes = narc_file.getFileByName("title.ntft")
-        extracted_image: Image = extract_image(image_file, colors)
+        palette = graphics2D.loadPalette(narc_file.getFileByName("title.ntfp"))
+        extracted_image: Image = extract_image(image_file, palette)
         if not output.endswith(".bmp"):
             output = f"{output}.bmp"
         extracted_image.save(output, compress_level=0)
