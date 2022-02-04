6badc9eef245df0b1220393440461501bc6ca1dc
Mike <mikebuntu68@gmail.com>
2022-02-04 00:08:57 -0500

    Move to `base/misc` folder

--- base/misc/title_screen.py
+++ base/misc/title_screen.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import click
 from ndspy import graphics2D, lz10, narc, rom
 
@@ -45,13 +45,34 @@ def insert_image(img: Image, palette: list):
 @click.option(
     "--image", type=str, required=False, help="(insert only) Image to replace title screen with"
 )
-def title_screen(insert: bool, input: str, output: str, image: str):
+@click.option("--version_string", type=str)
+def title_screen(insert: bool, input: str, output: str, image: str, version_string: str):
     nds_rom = rom.NintendoDSRom.fromFile(input)
     narc_file = narc.NARC(lz10.decompress(nds_rom.getFileByName("English/Menu/Tex2D/title.bin")))
     colors = graphics2D.loadPalette(narc_file.getFileByName("title.ntfp"))
 
     if insert:
         bmp = Image.open(image).convert("RGB")
+
+        if version_string:
+            draw = ImageDraw.Draw(bmp)
+            img_width, img_height = bmp.size
+            text_width, text_height = draw.textsize(version_string)
+            x = img_width - text_width - 2
+            y = img_height - text_height
+
+            # draw border around letters
+            stroke_color = (24, 66, 115)
+            draw.text((x - 1, y), version_string, fill=stroke_color)
+            draw.text((x + 1, y), version_string, fill=stroke_color)
+            draw.text((x, y - 1), version_string, fill=stroke_color)
+            draw.text((x, y + 1), version_string, fill=stroke_color)
+
+            # draw text
+            draw.text(
+                (x, y), version_string, fill=(214, 41, 41), stroke_width=4, stroke_fill=(0, 0, 0)
+            )
+
         new_image = insert_image(
             bmp,
             [
