8a19a59d5f2f7ca083fb861f28ef80a1add3c0c8
Mike <mikebuntu68@gmail.com>
2022-03-16 00:35:03 -0400

    Pin ndspy to 3.0.0 The recent 4.0.0 release introduced breaking changes that broke the `title_screen.py` script

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "ndspy",
+        "ndspy==3.0.0",
     ],
     extras_require={
         "test": [
