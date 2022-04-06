ddc0cb16cf823f80c7487e8fc77f1b4c329fa1f6
Mike <mikebuntu68@gmail.com>
2022-04-05 22:12:54 -0400

    Add dig spot randomization to patcher (not 100% working yet)

--- patcher/location_types/__init__.py
+++ patcher/location_types/__init__.py
@@ -1,8 +1,10 @@
+from .dig_spot import DigSpotLocation
 from .event import EventLocation
 from .island_shop import IslandShopLocation
 from .map_object import MapObjectLocation
 
 __all__ = [
+    "DigSpotLocation",
     "EventLocation",
     "IslandShopLocation",
     "MapObjectLocation",
