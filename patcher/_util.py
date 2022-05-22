b716a53d2a8d62d066537fb0a17917eb9e4a7701
Mike <mikebuntu68@gmail.com>
2022-05-21 23:41:40 -0400

    Fix type errors

--- patcher/_util.py
+++ patcher/_util.py
@@ -107,7 +107,6 @@ def patch_rom(aux_data: dict[Any, Any], input_rom: rom.NintendoDSRom) -> rom.Nin
                             raise NotImplementedError(f'Unknown location type "{other}"')
 
     # Write changes to the in-memory ROM
-    for cls in [EventLocation, IslandShopLocation, MapObjectLocation]:
-        cls.save_all()
+    Location.save_all()
 
     return input_rom
