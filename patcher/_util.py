e4dd808fd8eba959dcac024e3b2319a042163579
Mike <mikebuntu68@gmail.com>
2022-06-20 21:51:39 -0400

    Apply base patch in patcher

--- patcher/_util.py
+++ patcher/_util.py
@@ -1,21 +1,30 @@
+from io import BytesIO
 import json
 import os
 from pathlib import Path
 
 from ndspy import rom
+from vidua import bps
 
 from patcher._items import ITEMS
 from patcher.location_types import EventLocation, IslandShopLocation, Location, MapObjectLocation
 from shuffler.aux_models import Area, Chest, IslandShop, Npc, Tree
 
-# TODO: apply this to the input rom
 BASE_PATCH_PATH = Path(
-    os.environ.get("BASE_PATCH_PATH", Path(__file__).parent.parent / "patch.bps")
+    os.environ.get("BASE_PATCH_PATH", Path(__file__).parent.parent / "base" / "patch.bps")
 )
 
 
 def load_rom(file: Path):
-    input_rom = rom.NintendoDSRom.fromFile(str(file))
+    """
+    Load a ROM into memory, patch it, and return it as an ndspy NintendoDSRom object.
+
+    Note that the original file is not modified; the contents of the given ROM are copied into
+    memory, and the BPS patch is applied to that in-memory copy.
+    """
+    with open(BASE_PATCH_PATH, "rb") as patch_file:
+        patched_rom = bps.patch(source=BytesIO(file.read_bytes()), bps_patch=patch_file)
+    input_rom = rom.NintendoDSRom(data=patched_rom.read())
     Location.ROM = input_rom
     return input_rom
 
