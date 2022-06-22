a0f14979ec1c3f93fb224105fc8ad7b8c61e5182
Mike <mikebuntu68@gmail.com>
2022-06-21 22:59:48 -0400

    Add "talked to oshus for first time" flag

--- base/src/flags.h
+++ base/src/flags.h
@@ -1,2 +1,3 @@
 // Flags in format (offset_from_base_address, bit)
-#define MERCAY_BRIDGE_REPAIRED 2, 0x02
+#define MERCAY_BRIDGE_REPAIRED 0x2, 0x2
+#define TALKED_TO_OSHUS_FIRST_TIME 0x18, 0x2
--- base/src/set_initial_flags.c
+++ base/src/set_initial_flags.c
@@ -2,7 +2,7 @@
 #include "rando_settings.h"
 #include <stdint.h>
 
-static void set_flag(int addr, uint8_t bit) {
+static void set_flag(uint32_t addr, uint8_t bit) {
   *((uint8_t *)addr) |= bit;
 }
 
@@ -10,5 +10,7 @@ void set_initial_flags(uint32_t base_flag_address) {
   if (setting_is_enabled(MERCAY_BRIDGE_REPAIRED_FROM_START)) {
     set_flag(base_flag_address + MERCAY_BRIDGE_REPAIRED);
   }
+
+  set_flag(base_flag_address + TALKED_TO_OSHUS_FIRST_TIME);
   // TODO: finish documenting/setting the rest of the needed flags
 }
