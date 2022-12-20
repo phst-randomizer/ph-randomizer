0e941a61617cfb8c42b75bac17f1812210d8ebb0
Mike <mikebuntu68@gmail.com>
2022-12-19 23:29:44 -0500

    Decrease amount of repeated tests

--- tests/test_shuffler.py
+++ tests/test_shuffler.py
@@ -9,7 +9,7 @@ from shuffler.aux_models import Area
 from shuffler.logic import Edge, Logic, Node
 
 
-@pytest.mark.repeat(5)
+@pytest.mark.repeat(3)
 @pytest.mark.parametrize('seed', ['test', 'another_test', 'ANOTHER_TEST!!', 'this_is_a_real_seed'])
 def test_seeds(seed: str):
     """Test that running the shuffler with same seed multiple times produces identical aux data."""
