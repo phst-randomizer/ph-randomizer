ae4084d98e6fe0328b0ce0761deb2f0086814805
Mike <mikebuntu68@gmail.com>
2023-01-08 22:41:37 -0500

    Clean up flag patch comments

--- base/code/_flags.c
+++ base/code/_flags.c
@@ -4,14 +4,14 @@
 #define NO_SETTING -1, 0
 
 Flag flags[] = {
-    {0x0, 0x2, NO_SETTING},  // CELIA_TEXT_AFTER_GETTING_OSHUS_SWORD
-    {0x0, 0x4, NO_SETTING},  // COMPLETED_OSHUS_SWORD_TUTORIAL
-    {0x0, 0x8, NO_SETTING},  // TALKED_TO_BARTENDER_ABOUT_LINEBECK
-    {0x0, 0x20, NO_SETTING}, // TALKED_TO_OSHUS_AFTER_SEEING_RED_CHU_CHUS
-    {0x0, 0x40, NO_SETTING}, // SHOWED_LINEBECK_SW_SEA_CHART
-    {0x0, 0x80, NO_SETTING}, // SET_SAIL_FOR_THE_FIRST_TIME
-    {0x2, 0x2, MERCAY_BRIDGE_REPAIRED_FROM_START}, // MERCAY_BRIDGE_REPAIRED
+    {0x0, 0x2, NO_SETTING},  // celia dialogue after getting oshus sword
+    {0x0, 0x4, NO_SETTING},  // completed oshus sword tutorial
+    {0x0, 0x8, NO_SETTING},  // talked to mercay bartender about linebeck
+    {0x0, 0x20, NO_SETTING}, // talked to oshus after seeing red chuchus
+    {0x0, 0x40, NO_SETTING}, // showed linebeck the SW sea chart after ToTOK
+    {0x0, 0x80, NO_SETTING}, // set sail for the first time (linebeck dialog)
+    {0x2, 0x2, MERCAY_BRIDGE_REPAIRED_FROM_START}, // mercay bridge repaired
     {0x18, 0x2, NO_SETTING},                       // TALKED_TO_OSHUS_FIRST_TIME
-    {0x2c, 0x1, NO_SETTING},  // SAW_BROKEN_MERCAY_BRIDGE_FIRST_TIME
-    {0x26, 0x10, NO_SETTING}, // UNCHARTED_ISLAND_BRIDGE_EXTENDED
+    {0x2c, 0x1, NO_SETTING},  // saw broken mercay bridge for first time
+    {0x26, 0x10, NO_SETTING}, // uncharted island bridge is extended
 };
