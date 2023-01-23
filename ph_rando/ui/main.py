b207410b02f5c8cf41d1590b50a4a4cda5e92e8e
Mike <mikebuntu68@gmail.com>
2023-01-23 00:56:23 -0500

    Update setup.cfg script for running randomizer

--- ph_rando/ui/main.py
+++ ph_rando/ui/main.py
@@ -3,9 +3,14 @@ import sys
 from ph_rando.ui.cli import randomizer_cli
 from ph_rando.ui.gui import render_ui
 
-if __name__ == '__main__':
+
+def main() -> None:
     if '--no-gui' in sys.argv:
         sys.argv.remove('--no-gui')
         randomizer_cli()
     else:
         render_ui()
+
+
+if __name__ == '__main__':
+    main()
