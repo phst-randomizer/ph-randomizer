7d3ec0c4c5d424e1f8bfb92f8cb66a498e4b195e
Mike <mikebuntu68@gmail.com>
2023-03-03 23:09:22 -0500

    Seperate logic/aux validation tests from shuffler tests

--- tests/test_shuffler.py
+++ tests/test_shuffler.py
@@ -1,11 +1,6 @@
-from collections import defaultdict
-import json
-from pathlib import Path
-
 import pytest
 
 from ph_rando.shuffler import shuffle
-from ph_rando.shuffler.aux_models import Area
 from ph_rando.shuffler.logic import Edge, Logic, Node
 
 
@@ -18,17 +13,6 @@ def test_seeds(seed: str, settings):
     second = shuffle(seed, settings)
     third = shuffle(seed, settings)
     assert first == second == third
-
-
-def test_aux_data_validation():
-    """Run every aux data json through validation."""
-    aux_data_files = list(
-        (Path(__file__).parents[1] / 'ph_rando' / 'shuffler' / 'logic').rglob('*.json')
-    )
-    assert len(aux_data_files) > 0
-    for filename in aux_data_files:
-        with open(filename) as fd:
-            Area(**json.load(fd))
 
 
 @pytest.mark.parametrize(
@@ -114,46 +98,3 @@ def test_settings(expression: str, settings: dict[str, bool | str], expected_res
     edge = Edge(node1, node2, expression)
     node1.edges.append(edge)
     assert edge.is_traversable([]) == expected_result
-
-
-def test_graph_connectedness(settings) -> None:
-    logic = Logic(settings=settings)
-    logic.connect_rooms()
-
-    # Compute list of tuples of each check its area.
-    all_checks = [
-        (chest, area.name)
-        for area in logic.areas.values()
-        for room in area.rooms
-        for chest in room.chests
-    ]
-
-    # Put every item in the game in the current inventory, appending the area name to each
-    # small_key so we know which key goes to which area.
-    inventory = [
-        chest.contents if chest.contents != 'small_key' else f'small_key_{area_name}'
-        for chest, area_name in all_checks
-    ]
-
-    # Populate the `keys` dict for the assumed search function with all of
-    # the small keys in the inventory.
-    keys: dict[str, int] = defaultdict(int)
-    for item in inventory:
-        if item.startswith('small_key_'):
-            keys[item[len('small_key_') :]] += 1
-
-    reachable_nodes = logic.assumed_search(
-        logic.starting_node,
-        inventory,
-        keys,
-    )
-
-    # Create set consisting of all areas reported as reachable by the assumed search
-    reachable_areas = {node.area for node in reachable_nodes}
-
-    # Create set consisting of all areas in the logic
-    all_areas = {area.name for area in logic.areas.values()}
-
-    assert reachable_areas == all_areas
-
-    # TODO: assert that rooms + nodes are identical too
