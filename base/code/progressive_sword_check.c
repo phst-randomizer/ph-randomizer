764e7e4faed2bfc74b2134ba14d38964bceb82b3
Mike <mikebuntu68@gmail.com>
2023-04-21 01:39:41 -0400

    Update progressive sword patch
    - No longer rely on dynamic offset; instead, specify the full
      offsets for the sword flags from the dev sheet
    - Add Phantom Sword Blade as a part of the sword progression

--- base/code/progressive_sword_check.c
+++ base/code/progressive_sword_check.c
@@ -1,25 +1,36 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define OSHUS_SWORD_FLAG_OFFSET (0x128)
+#define OSHUS_SWORD_FLAG_OFFSET (0x21BA604)
 #define OSHUS_SWORD_FLAG_BIT (0x1)
-#define PHANTOM_SWORD_FLAG_OFFSET (0x12C)
+#define PHANTOM_SWORD_BLADE_FLAG_OFFSET (0x21B5550)
+#define PHANTOM_SWORD_BLADE_FLAG_BIT (0x20)
+#define PHANTOM_SWORD_FLAG_OFFSET (0x21BA608)
 #define PHANTOM_SWORD_FLAG_BIT (0x20)
 
-__attribute__((target("thumb"))) void progressive_sword_check(const uint32_t base_address) {
+__attribute__((target("thumb"))) void progressive_sword_check() {
   // Address of oshus sword flag
-  uint8_t *oshus_sword = (uint8_t *)(base_address + OSHUS_SWORD_FLAG_OFFSET);
+  uint8_t *oshus_sword = (uint8_t *)(OSHUS_SWORD_FLAG_OFFSET);
+
+  // Address of phantom sword blade flag
+  uint8_t *phantom_sword_blade = (uint8_t *)(PHANTOM_SWORD_BLADE_FLAG_OFFSET);
 
   // Address of phantom sword flag
-  uint8_t *phantom_sword = (uint8_t *)(base_address + PHANTOM_SWORD_FLAG_OFFSET);
+  uint8_t *phantom_sword = (uint8_t *)(PHANTOM_SWORD_FLAG_OFFSET);
 
   // Check if player has the oshus sword
   bool has_oshus_sword = (*oshus_sword & OSHUS_SWORD_FLAG_BIT) == OSHUS_SWORD_FLAG_BIT;
 
-  // If the player has the oshus sword already, give them the phantom sword.
-  // Otherwise, give them the oshus sword
-  if (has_oshus_sword) {
+  bool has_phantom_sword_blade =
+      (*phantom_sword_blade & PHANTOM_SWORD_BLADE_FLAG_BIT) == PHANTOM_SWORD_BLADE_FLAG_BIT;
+
+  // If the player has the oshus sword already, give them the phantom sword blade.
+  // If they have the phantom sword blade already, give them the phantom sword.
+  // Otherwise, give them the oshus sword.
+  if (has_oshus_sword && has_phantom_sword_blade) {
     *phantom_sword |= PHANTOM_SWORD_FLAG_BIT;
+  } else if (has_oshus_sword) {
+    *phantom_sword_blade |= PHANTOM_SWORD_BLADE_FLAG_BIT;
   } else {
     *oshus_sword |= OSHUS_SWORD_FLAG_BIT;
   }
