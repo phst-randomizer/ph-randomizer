834ffe162d0f3aeef00251e112b1e85e33a31a02
Mike <mikebuntu68@gmail.com>
2023-04-23 00:01:55 -0400

    Hook into progressive item check sooner

--- base/code/progressive_sword_check.c
+++ base/code/progressive_sword_check.c
@@ -8,19 +8,13 @@
 #define PHANTOM_SWORD_FLAG_OFFSET (0x21BA608)
 #define PHANTOM_SWORD_FLAG_BIT (0x20)
 
-__attribute__((target("thumb"))) void progressive_sword_check() {
-  // Address of oshus sword flag
+uint8_t progressive_sword_check(uint8_t item_id) {
+  // Addresses of oshus sword and phantom sword blade flags
   uint8_t *oshus_sword = (uint8_t *)(OSHUS_SWORD_FLAG_OFFSET);
-
-  // Address of phantom sword blade flag
   uint8_t *phantom_sword_blade = (uint8_t *)(PHANTOM_SWORD_BLADE_FLAG_OFFSET);
 
-  // Address of phantom sword flag
-  uint8_t *phantom_sword = (uint8_t *)(PHANTOM_SWORD_FLAG_OFFSET);
-
-  // Check if player has the oshus sword
+  // Check if player has the oshus sword or phantom sword blade
   bool has_oshus_sword = (*oshus_sword & OSHUS_SWORD_FLAG_BIT) == OSHUS_SWORD_FLAG_BIT;
-
   bool has_phantom_sword_blade =
       (*phantom_sword_blade & PHANTOM_SWORD_BLADE_FLAG_BIT) == PHANTOM_SWORD_BLADE_FLAG_BIT;
 
@@ -28,10 +22,10 @@ __attribute__((target("thumb"))) void progressive_sword_check() {
   // If they have the phantom sword blade already, give them the phantom sword.
   // Otherwise, give them the oshus sword.
   if (has_oshus_sword && has_phantom_sword_blade) {
-    *phantom_sword |= PHANTOM_SWORD_FLAG_BIT;
+    return 0x45;
   } else if (has_oshus_sword) {
-    *phantom_sword_blade |= PHANTOM_SWORD_BLADE_FLAG_BIT;
+    return 0x44;
   } else {
-    *oshus_sword |= OSHUS_SWORD_FLAG_BIT;
+    return 0x3;
   }
 }
