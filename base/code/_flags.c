0ca3d9af4cb025b2f160f1cabb5b93c04929db1b
Mike <mikebuntu68@gmail.com>
2023-11-04 19:10:16 -0400

    Format

--- base/code/_flags.c
+++ base/code/_flags.c
@@ -5,13 +5,13 @@
 
 Flag flags[] = {
     /* Mercay Island */
-    {0x28, 0x20, NO_SETTING},                      // celia dialogue explaining how to open door in oshus sword cave
-    {0x0, 0x2, NO_SETTING},                        // celia dialogue after getting oshus sword
-    {0x0, 0x4, NO_SETTING},                        // completed oshus sword tutorial
-    {0x0, 0x8, NO_SETTING},                        // talked to mercay bartender about linebeck
-    {0x0, 0x20, NO_SETTING},                       // talked to oshus after seeing red chuchus
-    {0x0, 0x40, NO_SETTING},                       // showed linebeck the SW sea chart after ToTOK
-    {0x0, 0x80, NO_SETTING},                       // set sail for the first time (linebeck dialog)
+    {0x28, 0x20, NO_SETTING}, // celia dialogue explaining how to open door in oshus sword cave
+    {0x0, 0x2, NO_SETTING},   // celia dialogue after getting oshus sword
+    {0x0, 0x4, NO_SETTING},   // completed oshus sword tutorial
+    {0x0, 0x8, NO_SETTING},   // talked to mercay bartender about linebeck
+    {0x0, 0x20, NO_SETTING},  // talked to oshus after seeing red chuchus
+    {0x0, 0x40, NO_SETTING},  // showed linebeck the SW sea chart after ToTOK
+    {0x0, 0x80, NO_SETTING},  // set sail for the first time (linebeck dialog)
     {0x2, 0x2, MERCAY_BRIDGE_REPAIRED_FROM_START}, // mercay bridge repaired
     {0x18, 0x2, NO_SETTING},                       // talked to Oshus first time
     {0x2c, 0x1, NO_SETTING},                       // saw broken mercay bridge for first time
