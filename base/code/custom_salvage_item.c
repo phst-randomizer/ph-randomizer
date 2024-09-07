1528a54bb804b0a46ef0241a1406c15b001a27e6
mike8699 <mikebuntu68@gmail.com>
2024-09-07 15:31:23 -0400

    Use built-in struct to set health

--- base/code/custom_salvage_item.c
+++ base/code/custom_salvage_item.c
@@ -1,3 +1,4 @@
+#include "ph.h"
 #include <stdint.h>
 
 #define MAX_HEALTH_ADDR 0x021BA348
@@ -7,15 +8,12 @@
 void custom_salvage_item(uint32_t item_id_with_flag) {
   uint16_t item_id = item_id_with_flag & 0x7FFF; // Unset the most significant bit
 
-  if (item_id == 0xA) { // Check if the item is a heart container
-    uint8_t *max_health_ptr = (uint8_t *)MAX_HEALTH_ADDR;
-    uint8_t *current_health_ptr = (uint8_t *)CURRENT_HEALTH_ADDR;
-
-    uint8_t max_health = *max_health_ptr;
-    max_health += 4; // Increment max health by 4 (one heart container)
-
-    *max_health_ptr = max_health;     // Update the max health
-    *current_health_ptr = max_health; // Restore the player's health to the new maximum
+  switch (item_id) {
+  case 0xA:
+    gHealthManager->mMaxHealth += 4; // Increment max health by 4 (one heart container)
+    gHealthManager->mHealth =
+        gHealthManager->mMaxHealth; // Restore the player's health to the new maximum
+    break;
   }
 
   uint32_t *player_health_ptr = (uint32_t *)PLAYER_HEALTH_PTR_ADDR;
